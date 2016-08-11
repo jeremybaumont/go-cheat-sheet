9ebc1e29ca1f75a590d645fae56a09fe358962ab
Lorin Hochstein <lhochstein@netflix.com>
2016-08-11 13:38:53 -0700

    Initial commit

---
+++ go.rb
@@ -0,0 +1,15 @@
+cheatsheet do
+    title 'Go'
+    docset_file_name 'Go'
+    keyword 'goc'
+    category do
+        id 'godoc'
+        entry do
+            name 'Docs for github repo'
+            notes <<-'END'
+            `http://godoc.org/github.com/<org>/<repo>`
+            END
+        end
+    end
+end
+
